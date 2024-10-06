d66c488c317edf352df0a3463a47f105f7348026
sharqawycs <sharqawycs@gmail.com>
2024-10-06 22:01:28 +0300

    Login function created

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,6 +23,28 @@ const registerUser = async (req, res) => {
 };
 
 //Login User
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+    const { username, password } = req.body;
 
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({ message: 'User Not Found' });
+        }
+
+        const isPassMatch = await bcrypt.compare(password, user.password);
+        if (!isPassMatch) {
+            return res.status(400).json({ message: 'Invalid Credentials' });
+        }
+
+        //todo: (WHY???) Create a JWT token
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '1h',
+        });
+
+        res.status(200).json({ message: 'Login successful', token });
+    } catch (error) {
+        res.status(500).json({ message: 'Error logging in', error });
+    }
+};
 module.exports = { registerUser, loginUser };
