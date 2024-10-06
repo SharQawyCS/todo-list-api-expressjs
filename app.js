9f9c3de764d019731ba62c6adb7ab33e990209e3
sharqawycs <sharqawycs@gmail.com>
2024-10-06 19:45:58 +0300

    Register User Function Created

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ require('dotenv').config();
 const app = express();
 
 //Middleware
-app.use(express.json);
+app.use(express.json());
 
 //connect to DB
-connectDB;
+connectDB();
+
+//Test route
+app.get('/', (req, res) => {
+    res.send('Welcome to the API');
+});
+
+//SERVER
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('Server Started on PORT:', PORT);
+});
---
+++ controllers/authController.js
@@ -0,0 +1,28 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jwt');
+
+//Register User
+const registerUser = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        const hashedPassword = bcrypt.hash(password, 10);
+
+        await User.create({ username, password: hashedPassword });
+
+        res.status(201).json({ message: 'User Registerd!' });
+    } catch (err) {
+        res.status(500).json({ message: 'error registering user', err });
+    }
+};
+
+//Login User
+const loginUser = async (req, res) => {};
+
+module.exports = { registerUser, loginUser };
