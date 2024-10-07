aeecd8ff759e3599b0407a7b44632ae88c3f334c
sharqawycs <sharqawycs@gmail.com>
2024-10-07 13:44:48 +0300

    user controller and router added #1

---
+++ controllers/userController.js
@@ -0,0 +1,3 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
---
+++ routes/userRoutes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const {} = require('../controllers/userController');
+const router = express.Router();
+
+module.exports = router;
